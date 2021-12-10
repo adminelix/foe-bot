4fdd4a45b84ec19148012e04c519d388cddb786b
felix bluethner <felix.bluethner@akelius.de>
2021-12-10 19:18:22 +0100

    add domain tests

---
+++ test/test_city_map.py
@@ -0,0 +1,16 @@
+import json
+
+from domain.city_map import CityMap
+
+
+def test_process():
+    f = open('content.json')
+    data = json.load(f)
+
+    startup_service_data = json.dumps(data[30]['responseData']['city_map'])
+    loads = json.loads(startup_service_data)
+
+    city = CityMap(**loads)
+
+    assert city.gridId == 'main'
+    assert city.entities[1].type == 'main_building'
---
+++ test/test_response_mapper.py
@@ -0,0 +1,13 @@
+import json
+
+from domain.account import Account
+from foe_bot.response_mapper import map as map_
+
+
+def test_map():
+    f = open('content.json')
+    data = json.load(f)
+
+    acc = map_(Account(), *data)
+
+    assert len(acc.city_map.entities) == 41
