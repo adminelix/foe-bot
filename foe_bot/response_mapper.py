0df07242058a4620eaa55f9f3674e928d83db094
felix bluethner <felix.bluethner@akelius.de>
2021-12-14 20:49:14 +0100

    format

--- foe_bot/response_mapper.py
+++ foe_bot/response_mapper.py
@@ -4,19 +4,25 @@ __ignored = ['TrackingService.trackLoginDone']
 
 
 def __map(acc: Account, **kwargs) -> None:
-    if 'StartupService' == kwargs['requestClass'] and 'getData' == kwargs['requestMethod']:
+    if ('StartupService' == kwargs['requestClass'] and 'getData' == kwargs['requestMethod']):
         acc.city_user_data.__init__(**kwargs['responseData']['user_data'])
         acc.city_map.__init__(**kwargs['responseData']['city_map'])
         acc.socket_connection_parameter.__init__(**kwargs['responseData']['socket_connection_parameter'])
+
     elif 'TimeService' == kwargs['requestClass'] and 'updateTime' == kwargs['requestMethod']:
         acc.time.__init__(**kwargs['responseData'])
+
     elif 'LogService' == kwargs['requestClass'] and 'setLogState' == kwargs['requestMethod']:
         acc.connection_state_logging.__init__(**kwargs['responseData'])
-    elif 'CityProductionService' == kwargs['requestClass'] and ('startProduction' == kwargs['requestMethod']
-                                                                or 'pickupProduction' == kwargs['requestMethod']):
+
+    elif ('CityProductionService' == kwargs['requestClass']
+          and ('startProduction' == kwargs['requestMethod']
+               or 'pickupProduction' == kwargs['requestMethod'])):
         acc.city_map.put_entities(*kwargs['responseData']['updatedEntities'])
+
     elif 'ResourceService' == kwargs['requestClass'] and 'getPlayerResources' == kwargs['requestMethod']:
         acc.resources.__init__(**kwargs['responseData']['resources'])
+
     else:
         class_method = f"{kwargs['requestClass']}.{kwargs['requestMethod']}"
         if class_method not in __ignored:
