5bbe6949f2b3013ba6b0384f70b47c4089c9671f
felix bluethner <felix.bluethner@akelius.de>
2021-12-19 11:40:23 +0100

    sample request and responses

---
+++ foe_bot/friends_tavern_service.py
@@ -0,0 +1,178 @@
+# class FriendsTavernService
+# getownTavern directly after collect to get tavern state
+
+
+# [
+#     {
+#         "__class__": "ServerRequest",
+#         "requestData": [],
+#         "requestClass": "FriendsTavernService",
+#         "requestMethod": "getOwnTavern",
+#         "requestId": 15
+#     }
+# ]
+#
+# [
+#     {
+#         "responseData": {
+#             "view": {
+#                 "tableLevel": 6,
+#                 "unlockedChairs": 16,
+#                 "visitors": [
+#                     {
+#                         "player_id": 849102670,
+#                         "name": "JuelzSantana",
+#                         "avatar": "portrait_id_171",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 365646,
+#                         "name": "Molchie",
+#                         "avatar": "addon_portrait_id_wildlife_halona",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 849404403,
+#                         "name": "Richard Eisenherz",
+#                         "avatar": "portrait_id_149",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 1285707,
+#                         "name": "pingiun",
+#                         "avatar": "portrait_id_24",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 7690382,
+#                         "name": "Agatha 737 die Mächtige",
+#                         "avatar": "portrait_id_142",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 9973421,
+#                         "name": "Balduin mit dem Beil",
+#                         "avatar": "addon_portrait_id_halloween_creepy_clown",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 849741992,
+#                         "name": "Zeno 602 der Krieger",
+#                         "avatar": "portrait_id_1",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 849566842,
+#                         "name": "Agrippa 870 der Zornige",
+#                         "avatar": "addon_portrait_id_jeoff",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 7670781,
+#                         "name": "Bobi Der Treue",
+#                         "avatar": "portrait_id_99",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 4151112,
+#                         "name": "Vogel91",
+#                         "avatar": "portrait_id_107",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 9901905,
+#                         "name": "Tamerlane",
+#                         "avatar": "addon_portrait_id_halloween_fortune",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 8711516,
+#                         "name": "Naiara die Loyale",
+#                         "avatar": "addon_portrait_id_makeda",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 849085821,
+#                         "name": "Darkside-DNA",
+#                         "avatar": "addon_portrait_id_skully",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 849095034,
+#                         "name": "Geronimus",
+#                         "avatar": "addon_portrait_id_pirate_king",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 5797899,
+#                         "name": "Eumel 13",
+#                         "avatar": "addon_portrait_id_soccer_fan_margaret",
+#                         "__class__": "BasePlayer"
+#                     },
+#                     {
+#                         "player_id": 850036569,
+#                         "name": "bau0",
+#                         "avatar": "portrait_id_10",
+#                         "__class__": "BasePlayer"
+#                     }
+#                 ],
+#                 "selectedCustomizationIds": {
+#                     "tablecloth": "tablecloth_5",
+#                     "tray": "tray_5",
+#                     "flooring": "flooring_5"
+#                 },
+#                 "tavernSilverBase": 62,
+#                 "tavernSilverAdd": 2,
+#                 "__class__": "TavernView"
+#             },
+#             "unlockedCustomizationIds": [
+#                 "tablecloth_0",
+#                 "tray_0",
+#                 "flooring_0",
+#                 "tablecloth_1",
+#                 "tablecloth_2",
+#                 "tablecloth_3",
+#                 "tablecloth_4",
+#                 "tray_1",
+#                 "flooring_1",
+#                 "tablecloth_5",
+#                 "tray_2",
+#                 "flooring_2",
+#                 "tray_3",
+#                 "flooring_3",
+#                 "tray_4",
+#                 "tray_5",
+#                 "flooring_4",
+#                 "flooring_5"
+#             ],
+#             "friendCount": 70,
+#             "__class__": "OwnTavern"
+#         },
+#         "requestClass": "FriendsTavernService",
+#         "requestMethod": "getOwnTavern",
+#         "requestId": 15,
+#         "__class__": "ServerResponse"
+#     }
+# ]
+#
+# [
+#     {
+#         "__class__": "ServerRequest",
+#         "requestData": [],
+#         "requestClass": "FriendsTavernService",
+#         "requestMethod": "collectReward",
+#         "requestId": 17
+#     }
+# ]
+#
+# [
+#     {
+#         "responseData": {
+#             "__class__": "Success"
+#         },
+#         "requestClass": "FriendsTavernService",
+#         "requestMethod": "collectReward",
+#         "requestId": 17,
+#         "__class__": "ServerResponse"
+#     }
+# ]
