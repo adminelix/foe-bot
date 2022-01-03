b53051cdddb6925fe26c7dc985c73c78ee74280d
felix bluethner <felix.bluethner@akelius.de>
2022-01-03 22:40:32 +0100

    add mapping ignore

--- foe_bot/response_mapper.py
+++ foe_bot/response_mapper.py
@@ -32,6 +32,7 @@ __ignored = [
     'RewardService.collectReward',  # reward type and amount
     'FriendsTavernService.getOtherTavern',  # details about friends tavern
     'MessageService.newMessage',  # hint that new message is available
+    'ChatService.messages',  # temporary to avoid log spamming
     'ChatService.message'  # temporary to avoid log spamming
 ]
 
