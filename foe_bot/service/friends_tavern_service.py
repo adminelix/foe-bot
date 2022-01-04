113169a3d1b4c30c1e55fc428ac5c4e2701d07c3
felix bluethner <felix.bluethner@akelius.de>
2022-01-04 13:10:01 +0100

    refactor

--- foe_bot/service/friends_tavern_service.py
+++ foe_bot/service/friends_tavern_service.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import time
 
 from foe_bot.domain.account import Account
 from foe_bot.request import Request
@@ -11,13 +12,17 @@ class FriendsTavernService:
         self.__acc = acc
         self.__request_session = Request()
         self.__logger = logging.getLogger("FriendsTavernService")
-        if not self.__acc.own_tavern:
-            self.refresh_own_tavern()
+        self.__last_refresh = 0
+        self.__refresh_interval = 15 * 60  # in seconds
+        self.__acc.own_tavern = None
+        self.__refresh_own_tavern(False)
 
-    def refresh_own_tavern(self):
-        request_body = self.__request_session.create_rest_body('FriendsTavernService', 'getOwnTavern', [])
-        response, _ = self.__request_session.send(request_body)
-        map_to_account(self.__acc, *response)
+    def __refresh_own_tavern(self, force: bool):
+        now = int(time.time())
+        if not self.__acc.own_tavern or force or now > self.__last_refresh + self.__refresh_interval:
+            request_body = self.__request_session.create_rest_body('FriendsTavernService', 'getOwnTavern', [])
+            response, _ = self.__request_session.send(request_body)
+            map_to_account(self.__acc, *response)
 
     def collect(self):
         own_tavern_state = self.__acc.other_tavern_states.get(self.__acc.city_user_data.player_id, None)
@@ -27,15 +32,15 @@ class FriendsTavernService:
             if success:
                 map_to_account(self.__acc, *response)
                 self.__logger.info("collected tavern rewards")
-                self.refresh_own_tavern()
+                self.__refresh_own_tavern(True)
 
     def visit(self):
         states = self.__acc.other_tavern_states
         player_ids_to_visit = [state.ownerId for state in states.values() if
                                not state.state and state.sittingPlayerCount < state.unlockedChairCount]
         visited = 0
-        for id in player_ids_to_visit:
-            request_body = self.__request_session.create_rest_body('FriendsTavernService', 'getOtherTavern', [id])
+        for id_ in player_ids_to_visit:
+            request_body = self.__request_session.create_rest_body('FriendsTavernService', 'getOtherTavern', [id_])
             response, success = self.__request_session.send(request_body)
             map_to_account(self.__acc, *response)
             sat_down = 'satdown' in json.dumps(response).lower()
