579d221a10215affff381829e51727bd24fd3504
felix bluethner <felix.bluethner@akelius.de>
2022-01-07 23:59:28 +0100

    fix bug that logservice does only send log at 300s connection time

--- foe_bot/foe_client/log_service.py
+++ foe_bot/foe_client/log_service.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import math
 import threading
 import time
 
@@ -39,7 +40,7 @@ class LogService(threading.Thread):
         reconnects = self.__ws_client.reconnects
         connection_time = self.__ws_client.connection_time
         # doubles interval if socket is connected longer than 900s
-        interval = self.__log_state_interval if connection_time < 900 else 2 * self.__log_state_interval
+        interval = self.__log_state_interval if connection_time < 1500 else 2 * self.__log_state_interval
 
         if reconnects >= 0 and connection_time > self.__last_log_state + interval:
             raw_body = self.__get_logstate_body()
@@ -50,7 +51,7 @@ class LogService(threading.Thread):
             response, _ = self.__request_session.send(body)
             map_to_account(self.__acc, *response)
 
-            self.__last_log_state = int(time.time())
+            self.__last_log_state = self.__round_down(connection_time)
             self.__logger.info(f'sent socketServer logState with connectedTime: {connection_time} '
                                f'and reconnects: {reconnects}')
 
@@ -88,3 +89,7 @@ class LogService(threading.Thread):
             "vram": 0
         }"""
         return json.loads(string)
+
+    @staticmethod
+    def __round_down(x) -> int:
+        return int(math.ceil(x / 100.0)) * 100
