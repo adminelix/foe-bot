4b4f6bb2cd53df265269398539ce6c2f3876000f
felix bluethner <felix.bluethner@akelius.de>
2022-01-07 22:45:11 +0100

    fix relative config file paths

--- foe_bot/__init__.py
+++ foe_bot/__init__.py
@@ -1,17 +1,21 @@
 import logging.config
+import os
 
 import yaml
 
+config_file = f"{os.path.dirname(os.path.realpath(__file__))}/../config.yml"
+logging_file = f"{os.path.dirname(os.path.realpath(__file__))}/../logging.conf"
+
 
 def load_config():
-    with open("config.yml", "r") as ymlfile:
+    with open(config_file, "r") as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.CLoader)
         return cfg
 
 
 cfg = load_config()
 
-logging.config.fileConfig('logging.conf')
+logging.config.fileConfig(logging_file)
 logging.getLogger("seleniumwire.handler").setLevel(logging.WARN)
 logging.getLogger("seleniumwire.server").setLevel(logging.WARN)
 logging.getLogger("urllib3.connectionpool").setLevel(logging.WARN)
