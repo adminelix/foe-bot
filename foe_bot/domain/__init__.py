6a2a09cd51aabe13d9ff00c9c630ea7457ad0e70
felix bluethner <felix.bluethner@akelius.de>
2022-01-09 09:28:47 +0100

    add diff to system time to Time object

--- foe_bot/domain/__init__.py
+++ foe_bot/domain/__init__.py
@@ -2,6 +2,8 @@ import cattr
 
 from foe_bot.domain.city_map import CityMap
 from foe_bot.domain.social_interaction_event import SocialInteractionEvent
+from foe_bot.domain.time import Time
 
+cattr.register_structure_hook(Time, lambda d, t: Time.serialize(**d))
 cattr.register_structure_hook(CityMap, lambda d, t: CityMap.serialize(**d))
 cattr.register_structure_hook(SocialInteractionEvent, lambda d, t: SocialInteractionEvent.serialize(**d))
--- foe_bot/domain/time.py
+++ foe_bot/domain/time.py
@@ -1,3 +1,5 @@
+import time as time_
+
 import attr
 
 
@@ -5,3 +7,14 @@ import attr
 class Time:
     time: int
     klass: str = attr.ib(default=None)
+
+    # custom field that stores system time when server time is received
+    sys_time: int = int(time_.time())
+
+    @staticmethod
+    def serialize(**kwargs):
+        return Time(sys_time=int(time_.time()), **kwargs)
+
+    @property
+    def diff(self):
+        return self.sys_time - self.time
