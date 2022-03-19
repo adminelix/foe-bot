6a2c9cd0d8359b716a1c4a852e76777352d4f7e3
felix bluethner <felix.bluethner@akelius.de>
2022-03-19 18:06:44 +0100

    #15 remove useless test

--- test/test_city_production_service.py
+++ test/test_city_production_service.py
@@ -1,15 +1,3 @@
-import re
-import sys
-
-
 def test_gather():
     # TODO mock Request() in CityProductionService() and test filter
     pass
-
-
-def test_foo():
-    bar: str = "nrerfv12"
-    foo = re.sub(r'[a-zA-Z]', '', bar)
-
-    print(foo)
-    sys.stderr.write(foo)
